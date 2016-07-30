6a7b5657129d0cf41affdc565495413c96d208f7
jess hust <jessicakhust@gmail.com>
2016-07-30 12:56:18 -0500

    Heroku connected

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var methodOverride = require('method-override'); 
 var bodyParser = require('body-Parser');
 var app = express(); 
-
-var PORT = process.env.PORT || 3000; 
+ 
 
 var exphbs = require('express-handlebars');
 
@@ -30,4 +29,4 @@ var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes); 
 
  
-app.listen(PORT); 
+app.listen(process.env.PORT || 3000); 
