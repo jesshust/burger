d6274a32aac9bab49f35ad7f8b663bcdcffff7f5
jess hust <jessicakhust@gmail.com>
2016-07-30 14:19:50 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -8,12 +8,12 @@ if (process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else {
 	connection = mysql.createConnection({	 
-	host: 'localhost', 
+	host:'localhost', 
 	user: 'root', 
 	password: '',
 	database: 'burgers_db'
 	})
-}  
+};  
 
 connection.connect(); 
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');  
 var methodOverride = require('method-override'); 
-var bodyParser = require('body-Parser');
+var bodyParser = require('body-parser');
 var app = express(); 
  
 
