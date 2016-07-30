2a575b0fb60fb50b6e12b5246c5d84e79ca59805
jess hust <jessicakhust@gmail.com>
2016-07-30 13:37:25 -0500

    Update

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ var connection;
 if (process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else {
-	connection = mysql.createConnection ({	 
-	host: process.env.db_host || 'localhost', 
-	user: process.env.db_user || 'root', 
-	password: process.env.db_pw || '',
-	database: process.env.db || 'burgers_db'
+	connection = mysql.createConnection({	 
+	host:'localhost', 
+	user: 'root', 
+	password: '',
+	database:'burgers_db'
 	})
 };  
 
