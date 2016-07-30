e4ef9a78763cd5fbee62126881e6f425ff78b395
jess hust <jessicakhust@gmail.com>
2016-07-30 12:44:37 -0500

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,21 @@
 //setup the code to connect Node to MySQL
 var mysql = require('mysql'); 
 
-//port readable for heroku
-var PORT = process.env.PORT || 3306; 
 
-var connection = mysql.createConnection({ 
+var connection; 
+
+if (process.env.JAWSDB_URL){
+	connection= mysql.createConnection(process.env.JAWSDB_URL); 
+} else{
+	connection = mysql.createConnection({	 
 	host: process.env.db_host || 'localhost', 
 	user: process.env.db_user || 'root', 
 	password: process.env.db_pw || '',
 	database: process.env.db || 'burgers_db'
-}); 
+	});
+};  
 
-connection.connect(function(err){
-	if(err){
-		console.error('error connecting: ' + err.stack); 
-		return; 
-	}
-
-	console.log('connected as id ' + connection.threadId); 
-}); 
+connection.connect(); 
 
 //export the connection 
 module.exports = connection; 
